f441b87340eb022d869b15fbbcabb4d7bb715f8b
wester08 <wester.feliz@gmail.com>
2025-06-18 20:19:11 -0400

    Actualizado

---
+++ BiblioSol.Domain/Entities/Dashboard.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BiblioSol.Domain.Base;
+
+namespace BiblioSol.Domain.Entities
+{
+    [Table("Dashboard", Schema = "Biblioteca")]
+    public class Dashboard : AuditEntity
+    {
+        [Key]
+        public int idActividad { get; set; }
+        public string AccionRealizada { get; set; }
+        public string Descripcion { get; set; }
+
+
+    }
+}
---
+++ BiblioSol.Domain/Entities/PlantillaNotificaciones.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BiblioSol.Domain.Base;
+
+namespace BiblioSol.Domain.Entities
+{
+    [Table("PlantillaNotificaciones", Schema = "Biblioteca")]
+    public class PlantillaNotificaciones : AuditEntity
+    {
+        [Key]
+        public int idPlantilla { get; set; }
+        public int tipoNotificacionId { get; set; }
+        public string asunto { get; set; }
+        public string cuerpo { get; set; } = string.Empty;
+
+
+    }
+}
